1ba11c02014f0e7c8cab43b615eddf28faac6bba
Adam Mulani <aadammulani12@gmail.com>
2024-02-29 13:24:31 +0530

    written logic to display who wins, with background color changing of msg

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let uscore=0;  //user score
 let cscore=0;  //computer score
 
 const uchoices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
 
 const computersTurn=()=>{
     const choices=["rock","paper","scissors"];
@@ -10,15 +11,64 @@ const computersTurn=()=>{
    return choices[randomIndex];
 }
 
+const gameDraw=()=>{
+    console.log("Game draw");
+    msg.innerText="Game draw. Play again";
+    msg.style.backgroundColor="#003566";
+}
+
+const showWinner=(userwin,userchoice,compChoice)=>{
+    if(userwin)
+    {
+        console.log("!! You win ");
+        msg.innerText=`!! You win , Your ${userchoice} beats ${compChoice}`;
+        msg.style.backgroundColor="#155d27";
+    }
+    else{
+        console.log("---You lost the game---")
+        msg.innerText=msg.innerText=`!! You lose ,${compChoice} beats  your ${userchoice}`;
+        msg.style.backgroundColor="#c1121f";
+
+
+    }
+}
+
 const game=(choice)=>{
-  console.log("users choice ",choice.getAttribute("id"));
+  //console.log("users choice ",choice.getAttribute("id"));
   // generate computers choice
+
+  console.log("users choice ",choice);
   const compChoice=computersTurn();
   console.log("Computers choice ", compChoice);
+
+  if(choice===compChoice)
+  {
+    // game draw
+    gameDraw();
+  }
+  else{
+    let userwin=true;
+    if(choice==="rock")  ///users choice is rock
+    {
+        // now here computer choice can be either scissors or paper
+        userwin= (compChoice==="scissors")?true:false;
+    }
+    else if(choice==="paper")
+    {
+        // here computer choice can be either rock or scissors
+        userwin=(compChoice==="rock")?true:false;
+    }
+    else if(choice==="scissors")
+    {
+        //here computer choice can be either paper or rock
+        userwin=(compChoice==="paper")?true:false;
+    }
+    showWinner(userwin,choice,compChoice);
+  }
 }
 
 uchoices.forEach((choice)=>{
     choice.addEventListener("click",()=>{
-       game(choice);
+       game(choice.getAttribute("id"));
     })
 })
